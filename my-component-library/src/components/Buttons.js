36e62159d444ac6f0d74de5f1851cf2fc6eaec44
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-02-04 19:57:31 -0300

    aplication setup

---
+++ my-component-library/src/components/Buttons.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+    background-color: red;
+    border: none;
+    color: white;
+    padding; 12px 24px;
+    font-size: 1rem;
+`;
+
+export default PrimaryButton;
