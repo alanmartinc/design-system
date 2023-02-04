f3484dd6439b2214d9b2cbc821fb557ab0ecfc8d
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-02-04 20:23:38 -0300

    button activity exercise

--- my-component-library/src/components/Buttons.js
+++ my-component-library/src/components/Buttons.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
-const PrimaryButton = styled.button`
-    background-color: red;
-    border: none;
-    color: white;
+const primaryBlue = "#030086";
+
+const Button = styled.button`
     padding; 12px 24px;
     font-size: 1rem;
+    border-radius: 2px;
+    min-width: 100px;
+    cursor: pointer;
+    font-family: "Roboto Mono", monospace;
+`;
+
+const PrimaryButton = styled(Button)`
+    background-color: ${primaryBlue};
+    border: none;
+    color: white;
 `;
 
 export default PrimaryButton;
