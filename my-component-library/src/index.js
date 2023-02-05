1b2742816ae1dadc846282e29f010e855e0e61f0
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-02-05 01:34:56 -0300

    adding global styles

--- my-component-library/src/index.js
+++ my-component-library/src/index.js
@@ -4,12 +4,14 @@ import PrimaryButton, {
   SecondaryButton,
   TertiaryButton,
 } from "./components/Buttons";
+import { GlobalStyle } from "./utils";
 
 const App = () => (
   <div>
     <PrimaryButton>Hello world</PrimaryButton>
     <SecondaryButton>Goodbye world</SecondaryButton>
     <TertiaryButton>Hey world</TertiaryButton>
+    <GlobalStyle />
   </div>
 );
 
