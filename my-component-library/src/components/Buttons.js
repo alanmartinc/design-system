e4f59ff738da1f5032e3a96ca1ea67e93500fce9
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-02-05 01:20:24 -0300

    creating a theme

--- my-component-library/src/components/Buttons.js
+++ my-component-library/src/components/Buttons.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
-
-const primaryBlue = "#030086";
+import { defaultTheme, typeScale } from "../utils";
 
 const Button = styled.button`
   padding: 12px 24px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
@@ -12,21 +11,21 @@ const Button = styled.button`
 `;
 
 const PrimaryButton = styled(Button)`
-  background-color: ${primaryBlue};
+  background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
 `;
 
 export const SecondaryButton = styled(Button)`
   background-color: none;
-  border: 2px solid ${primaryBlue};
-  color: ${primaryBlue};
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export const TertiaryButton = styled(Button)`
   background-color: none;
   border: none;
-  color: ${primaryBlue};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export default PrimaryButton;
---
+++ my-component-library/src/utils/themes.js
@@ -0,0 +1,12 @@
+import { blue, neutral } from "./colors";
+import { primaryFont } from "./typography";
+
+export const defaultTheme = {
+  primaryColor: blue[300],
+  primaryColorHover: blue[200],
+  primaryColorActive: blue[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont,
+};
